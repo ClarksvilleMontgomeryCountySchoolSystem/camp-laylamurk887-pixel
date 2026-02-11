9568b9f75ce4e94efe5483872d575edf1845ce06
laylamurk887-pixel <layla.murk887@students.cmcss.net>
2026-02-11 08:46:47 -0600

    Hellooooooooooooooooo

--- task2.py
+++ task2.py
@@ -7,34 +7,34 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-
+first = str(input())
 
 # last
-
+last = str(input())
 
 # birth
-
+birth = str(input())
 
 # days
-
+days = int(input())
 
 # p_first
-
+p_first = str(input())
 
 # p_last
-
+p_last = str(input())
 
 # phone
-
+phone = int(input())
 
 # street
-
+street = str(input())
 
 # city
-
+city = str(input())
 
 # state
-
+state = str(input())
 
 # zip_code
-
+zip = int(input())
